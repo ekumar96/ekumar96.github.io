4e180bc9596aeb32ae768d1b1eb1a2311b9b18c8
Eshan Kumar <36972518+ekumar96@users.noreply.github.com>
2023-11-14 19:42:15 -0800

    Add Navbar highlight

--- app.js
+++ app.js
@@ -12,6 +12,41 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
+
+// show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const projectsMenu = document.querySelector('#projects-page')
+    let scrollPos = window.scrollY
+
+    // Adds 'highlight' class to menu items - class should only work if not in mobile
+    if(window.innerWidth > 960 && scrollPos < 600) { // 600 is when we should be on home highlighted
+        homeMenu.classList.add('highlight')
+        projectsMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        homeMenu.classList.remove('highlight')
+        aboutMenu.classList.remove('highlight')
+        projectsMenu.classList.add('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 3545) {
+        aboutMenu.classList.add('highlight')
+        projectsMenu.classList.remove('highlight')
+        return
+    }
+
+
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight')
+
+    }
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
 /// Close mobile Menu when click on menu item
 const hideMobileMenu = () => {
     const menuBars = document.querySelector('.is-active') // menu list is down
